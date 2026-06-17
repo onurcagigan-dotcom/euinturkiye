5e62d288db13e4ccb269e0867bdaf03c61f93178
onurcagigan-dotcom <onurcagigan@gmail.com>
2026-06-17 10:04:49 +0300

    Delete lib directory

--- lib/not-found-helper.ts
+++
@@ -1,8 +0,0 @@
-import { notFound } from "next/navigation";
-
-/** TypeScript'in notFound()'u never olarak tanımaması sorununu çözer */
-export function notFoundNever(): never {
-  notFound();
-  // Aşağıdaki satır çalışmaz ama TypeScript'i memnun eder
-  throw new Error("notFound");
-}
